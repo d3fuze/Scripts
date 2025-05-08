4196b13686e413fba7eb32568ab602b91474846a
d3fuze <58686863+d3fuze@users.noreply.github.com>
2025-05-08 18:32:58 -0400

    Update zsh_config.sh
    This will install OhMyZSH along with the syntax highlighting and auto suggestions plugins on a linux system which is already using ZSH.

--- zsh_config.sh
+++ zsh_config.sh
@@ -1,1 +1,12 @@
+mkdir ~/gh
+cd ~/gh
+git clone https://github.com/ohmyzsh/ohmyzsh.git
+cd ohmyzsh
+~/gh/ohmyzsh/tools/install.sh
+cd ~/gh
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
+echo "source ${(q-)PWD}/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+source ~/.zshrc
 
